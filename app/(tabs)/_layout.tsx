2d1278a91f29d5892e62fc763ff6f57082e03176
iusenenow <iusenenow@gmail.com>
2026-04-05 11:28:47 +1000

    feat: project setup, nativewind, routing, tab navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,57 @@
+import {Tabs} from "expo-router";
+import {tabs} from "@/constants/data";
+import {View, Image} from "react-native";
+import clsx from "clsx";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+import {components, colors} from "@/constants/theme";
+
+const tabBar = components.tabBar
+
+const TabLayout = () => {
+	const insets = useSafeAreaInsets();
+
+	const TabIcon = ({focused, icon}: TabIconProps) => {
+		return <View className="tabs-icon">
+			<View className={clsx('tabs-pill', focused && 'tabs-active')}>
+				<Image source={icon} resizeMode="contain" className="tabs-glyph"/>
+			</View>
+		</View>
+	}
+
+	return <Tabs
+		screenOptions={{
+			headerShown: false,
+			tabBarShowLabel: false,
+			tabBarStyle: {
+				position: 'absolute',
+				bottom: Math.max(insets.bottom, tabBar.horizontalInset),
+				height: tabBar.height,
+				marginHorizontal: tabBar.horizontalInset,
+				borderRadius: tabBar.radius,
+				backgroundColor: colors.primary,
+				borderTopWidth: 0,
+				elevation: 0,
+			},
+			tabBarItemStyle: {
+				paddingVertical: tabBar.height / 2 - tabBar.iconFrame / 1.6
+			},
+			tabBarIconStyle: {
+				width: tabBar.iconFrame,
+				height: tabBar.iconFrame,
+				alignItems: 'center',
+			}
+		}}>
+		{tabs.map((tab) => (
+			<Tabs.Screen
+				key={tab.name}
+				name={tab.name}
+				options={{
+					title: tab.title,
+					tabBarIcon: ({focused}) => <TabIcon focused={focused} icon={tab.icon}></TabIcon>
+				}}
+			/>
+		))}
+	</Tabs>
+}
+
+export default TabLayout;
--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,15 @@
 import {Text, View} from 'react-native';
+import {SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "nativewind";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const Insights = () => {
-    return (
-        <View>
-            <Text>Insights</Text>
-        </View>
-    )
+	return (
+		<SafeAreaView className="flex-1 p-5 bg-background">
+			<Text>Insights</Text>
+		</SafeAreaView>
+	)
 }
 
 export default Insights;
